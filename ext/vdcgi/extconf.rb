1c68e7060131b51c7795fc5e5e63f408ad24c5f0
aleks <aleks@vserver.vn.wr>
2025-03-26 19:33:26 +0300

    26_03_2025

--- ext/vdcgi/extconf.rb
+++ ext/vdcgi/extconf.rb
@@ -5,10 +5,9 @@ require "mkmf"
 # Makes all symbols private by default to avoid unintended conflict
 # with other gems. To explicitly export symbols you can use RUBY_FUNC_EXPORTED
 # selectively, or entirely remove this flag.
-# append_cflags("-fvisibility=hidden")
-require "fileutils"
+append_cflags("-fvisibility=hidden")
 extension_name = 'vdcgi'
-v = " -std=c11 "
+v = " -std=c17 "
 $defs.push(v)
 $warnflags = "-Wall"
 $libs += " -lmysqlclient -lcrypto -ljansson -lssl -lpthread  -lpthread -ldl -lcrypt -lm  -lc "
